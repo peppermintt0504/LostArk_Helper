21932887d8dcc79d5ccde5721bb2cb95fcf9e639
Yooooodong <73277351+peppermintt0504@users.noreply.github.com>
2023-05-09 17:30:17 +0900

    make project & stone page preset

---
+++ src/App.vue
@@ -0,0 +1,88 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+import './assets/global.css'
+</script>
+
+<template>
+  <div class="layout">
+    <header class="center">
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/stone">stone</RouterLink>
+      <RouterLink to="/raid-box">raid-box</RouterLink>
+    </nav>
+  </header>
+  <main>
+    <RouterView/>
+  </main>
+  </div>
+  
+
+</template>
+
+<style scoped>
+
+header {
+  line-height: 1.5;
+  height: 50px;
+  background-color: slategray;
+}
+
+main{
+  margin-top: 50px;
+}
+
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+@media (min-width: 1024px) {
+  header {
+    position: absolute;
+    top : 0;
+    left:  0;
+    display: flex;
+    width: 100vw;
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
+  }
+}
+</style>
---
+++ src/views/StoneView.vue
@@ -0,0 +1,62 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="pageLayout center">
+    <div class="stonePage">
+      <img src="../assets/image/stoneScreen.png">
+      <!-- <img class="debuffActive" src="../assets/image/감보석.png">
+      <img class="buffFirstActive" src="../assets/image/보석.png">
+      <img class="buffSecendActive" src="../assets/image/보석.png"> -->
+      <img class="buffFirst" src="../assets/image/선수필승.png">
+      <img class="buffFirstText" src="../assets/image/선수필승_텍스트.png">
+      <img class="buffSecend" src="../assets/image/실드_관통.png">
+      <img class="buffSecendText" src="../assets/image/실드_관통_텍스트.png">
+    </div>
+  </div>
+</template>
+
+<style>
+.stonePage{
+  height: 100%;
+  position: relative;
+}
+.debuffActive{
+  position: absolute;
+  right: 123px;
+  bottom: 217px;
+}
+.buffFirstActive{
+  position: absolute;
+  right: 123px;
+  bottom: 437px;
+}
+.buffSecendActive{
+  position: absolute;
+  right: 123px;
+  bottom: 344px;
+}
+.buffFirst{
+  position: absolute;
+  left: 13px;
+  bottom: 384px;
+}
+.buffFirstText{
+  position: absolute;
+  left: 104px;
+  bottom: 437px;
+}
+.buffSecend{
+  position: absolute;
+  left: 13px;
+  bottom: 291px;
+}
+.buffSecendText{
+  position: absolute;
+  left: 104px;
+  bottom: 345px;
+}
+@media (min-width: 1024px) {
+  
+}
+</style>
