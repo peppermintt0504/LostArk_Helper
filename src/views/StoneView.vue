ff57b0c19e7d8213b72e12f5c51b6e19b4ccb311
Yooooodong <73277351+peppermintt0504@users.noreply.github.com>
2023-05-11 16:42:50 +0900

    세공 기능 일부 완성

--- src/views/StoneView.vue
+++ src/views/StoneView.vue
@@ -4,17 +4,64 @@
   export default {
     props: {
     },
-    data () {
-      return {
-
-        buffAct1 : [0,1,1,0,1,0,1,1,1,1],
-        buffAct2 : [0,1,0,1,1,0,1,0,1,1],
-        
-      }
-    },
     setup(){
+      const buffAct1 = ref(Array(10).fill(0));
+      const buffAct2 = ref(Array(10).fill(0));
+      const debuffAct = ref(Array(10).fill(0));
+
+      let percentage = ref(75);
+
       let buff1 = ref(0);
       let buff2 = ref(0);
+
+      let firstBuffCount = ref(0);
+      let secendBuffCount = ref(0);
+      let deBuffCount = ref(0);
+
+      function tryWork (type) {
+        switch (type) {
+          case 'first' : {
+            if(firstBuffCount.value === 10) break;
+            const result = parseInt(Math.random()*100)
+            if(result <= percentage.value){
+              buffAct1.value[firstBuffCount.value] = 1;
+              percentage.value = percentage.value === 25 ? 25 :percentage.value - 10;
+            }else{
+              buffAct1.value[firstBuffCount.value] = 2;
+              percentage.value = percentage.value === 75 ? 75 :percentage.value + 10;
+            }
+            firstBuffCount.value++;
+            break;
+          }
+          case 'secend' : {
+            if(secendBuffCount.value === 10) break;
+            const result = parseInt(Math.random()*100)
+            if(result <= percentage.value){
+              buffAct2.value[secendBuffCount.value] = 1;
+              percentage.value = percentage.value === 25 ? 25 :percentage.value - 10;
+            }else{
+              buffAct2.value[secendBuffCount.value] = 2;
+              percentage.value = percentage.value === 75 ? 75 :percentage.value + 10;
+            }
+            secendBuffCount.value++;
+            break;
+          }
+          case 'debuff' : {
+            if(deBuffCount.value === 10) break;
+            const result = parseInt(Math.random()*100)
+            if(result <= percentage.value){
+              debuffAct.value[deBuffCount.value] = 1;
+              percentage.value = percentage.value === 25 ? 25 :percentage.value - 10;
+            }else{
+              debuffAct.value[deBuffCount.value] = 2;
+              percentage.value = percentage.value === 75 ? 75 :percentage.value + 10;
+            }
+            deBuffCount.value++;
+            break;
+          }
+        }
+      }
+
       function changebuff (index){
         index === 1 ? buff1.value = (buff1.value + 1) % buffList.length : buff2.value = (buff2.value + 1) % buffList.length;
       }
@@ -32,7 +79,12 @@
         getTextURL,
         activeURL,
         changebuff,
+        tryWork,
+        percentage,
         buffList,
+        buffAct1,
+        buffAct2,
+        debuffAct,
         buff1,
         buff2
       }
@@ -41,23 +93,46 @@
 </script>
 
 <template>
-  <div class="pageLayout center">
+  <div class="pageLayout center" oncopy="return false" oncut="return false" onpaste="return false">
     <div class="stonePage">
       <img src="../assets/image/stoneScreen.png">
-      <!-- <img class="debuffActive" src="../assets/image/감보석.png">
-      <img class="buffFirstActive" src="../assets/image/보석.png">
-      <img class="buffSecendActive" src="../assets/image/보석.png"> -->
+      <img class="debuffActive" :style="{display : (debuffAct[0] === 0 ? 'none' : '')}" src="../assets/image/감보석.png">
+      <div class="debuffActiveCount" :style="{display : (debuffAct[0] === 0 ? 'none' : '')}">{{ debuffAct.reduce((x,v)=>v === 1?x+1:x,0) }}</div>
+
+      <img class="buffFirstActive" :style="{display : (buffAct1[0] === 0 ? 'none' : '')}" src="../assets/image/보석.png">
+      <div class="buffFirstActiveCount" :style="{display : (buffAct1[0] === 0 ? 'none' : '')}">{{ buffAct1.reduce((x,v)=>v === 1?x+1:x,0) }}</div>
+
+      <img class="buffSecendActive" :style="{display : (buffAct2[0] === 0 ? 'none' : '')}" src="../assets/image/보석.png">
+      <div class="buffSecendActiveCount" :style="{display : (buffAct2[0] === 0 ? 'none' : '')}">{{ buffAct2.reduce((x,v)=>v === 1?x+1:x,0) }}</div>
+
       <img v-on:click="changebuff(1)" class="buffFirst" :src="getImageURL(buffList[buff1])">
       <img class="buffFirstText" :src="getTextURL(buffList[buff1])">
       <img v-on:click="changebuff(2)" class="buffSecend" :src="getImageURL(buffList[buff2])">
       <img class="buffSecendText" :src="getTextURL(buffList[buff2])">
-      <img v-for="(buff,index) in buffAct1" class="buffFirstActiveStone" :style="{ left : `${109 + (40 * index)}px` }" :src="activeURL(buff ? 'active' : 'fail')">
-      <img v-for="(buff,index) in buffAct2" class="buffSecendActiveStone" :style="{ left : `${109 + (40 * index)}px` }" :src="activeURL(buff ? 'active' : 'fail')">
+      <img v-for="(buff,index) in buffAct1" class="buffFirstActiveStone" :style="{ left : `${109 + (40 * index)}px`,display : (buff === 0 ? 'none' : '') }" :src="activeURL(buff === 1 ? 'active' : 'fail')">
+      <img v-for="(buff,index) in buffAct2" class="buffSecendActiveStone" :style="{ left : `${109 + (40 * index)}px`,display : (buff === 0 ? 'none' : '') }" :src="activeURL(buff === 1 ? 'active' : 'fail')">
+      <img v-for="(buff,index) in debuffAct" class="debuffActiveStone" :style="{ left : `${109 + (40 * index)}px`,display : (buff === 0 ? 'none' : '') }" :src="activeURL(buff === 1 ? 'active' : 'fail')">
+      <div class="firstButton" v-on:click="tryWork('first')"></div>
+      <div class="secendButton" v-on:click="tryWork('secend')"></div>
+      <div class="debufButton" v-on:click="tryWork('debuff')"></div>
+      
+      
+      <div class="buffFirstpercentage">
+        <span class="percentageText">성공 확률 </span>
+        <span class="percentage">{{percentage}}</span>
+        <span class="percentageEnd">%</span>
+      </div>
+      <div class="debuffpercentage">
+        <span class="percentageText">균열 확률 </span>
+        <span class="percentage">{{percentage}}</span>
+        <span class="percentageEnd">%</span>
+      </div>
     </div>
   </div>
 </template>
 
 <style>
+*{-webkit-user-drag: none;}
 .stonePage{
   height: 100%;
   position: relative;
@@ -65,7 +140,28 @@
 .debuffActive{
   position: absolute;
   right: 123px;
-  bottom: 217px;
+  top: 489.5px;
+}
+.debuffActiveCount{
+  position: absolute;
+  right: 142px;
+  top: 489px;
+  color : #af4440;
+  filter: blur(0.5px);
+}
+.buffFirstActiveCount{
+  position: absolute;
+  right: 142px;
+  top: 268px;
+  color : #01acf3;
+  filter: blur(0.5px);
+}
+.buffSecendActiveCount{
+  position: absolute;
+  right: 142px;
+  top: 362px;
+  color : #01acf3;
+  filter: blur(0.5px);
 }
 .buffFirstActive{
   position: absolute;
@@ -106,6 +202,59 @@
   position: absolute;
   top: 387px;
   left: 109px;
+}
+.debuffActiveStone{
+  position: absolute;
+  top: 514px;
+  left: 109px;
+}
+.firstButton{
+  position: absolute;
+  right: 26px;
+  top: 273px;
+  opacity: 0.3;
+  width: 92px;
+  height: 65px;
+}
+.secendButton{
+  position: absolute;
+  right: 26px;
+  top: 365px;
+  opacity: 0.3;
+  width: 92px;
+  height: 65px;
+}
+.debufButton{
+  position: absolute;
+  right: 26px;
+  top: 492px;
+  opacity: 0.3;
+  width: 92px;
+  height: 65px;
+}
+.buffFirstpercentage{
+  position: absolute;
+  top : 235px;
+  right: 20px;
+  filter: blur(0.5px);
+}
+.debuffpercentage{
+  position: absolute;
+  top : 455px;
+  right: 20px;
+  filter: blur(0.5px);
+}
+.percentageText{
+  color : white;
+  font-size: 15px;
+}
+.percentage{
+  color : #d3ae04f2;
+  font-size: 18px;
+}
+.percentageEnd{
+  color : #d3ae04f2;
+  font-size: 18px;
 }
 @media (min-width: 1024px) {
   
